a6fa4ce8bf663e56be235966dbb42ee1a68421f7
ZetlJ2004 <1374691379@qq.com>
2024-09-18 16:52:34 +0800

    exp1

--- jdbc-examples/src/main/java/org/example/jdbcexamples/repository/AddressRepository.java
+++ jdbc-examples/src/main/java/org/example/jdbcexamples/repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package org.example.jdbcexamples.repository;
 
 import org.example.jdbcexamples.dox.Address;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,22 @@ public interface AddressRepository extends CrudRepository<Address, String> {
             where a.user_id = :userId
             """)
     List<Address> findByUserId(String userId);
+
+    @Modifying
+    @Query("""
+            delete from address a where a.user_id = :userId
+            """)
+    void deleteByUserId(String userId);
+
+    @Modifying
+    @Query("""
+            update address a set a.detail=:detail where a.id=:id
+            """)
+    void updateDetail(String detail, String id);
+
+    @Modifying
+    @Query("""
+            update address a set a.detail=:detail where a.user_id=:id
+            """)
+    void updateDetailUser(String detail, String id);
 }
--- jdbc-examples/src/test/java/org/example/jdbcexamples/repository/AddressRepositoryTest.java
+++ jdbc-examples/src/test/java/org/example/jdbcexamples/repository/AddressRepositoryTest.java
@@ -5,6 +5,7 @@ import org.example.jdbcexamples.dox.Address;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,5 +19,25 @@ class AddressRepositoryTest {
         for (Address address : addressRepository.findByUserId("1285832708231036928")) {
             log.debug("address: {}", address);
         }
+    }
+    @Test
+    void deleteById(){
+        addressRepository.deleteById("4");
+    }
+
+    @Test
+    void deleteByUserId(){
+        addressRepository.deleteByUserId("1256");
+        log.debug("Deleted addresses for userId 1256");
+    }
+
+    @Test
+    void updateDetail(){
+        addressRepository.updateDetail("1256","4");
+    }
+
+    @Test
+    void updateDetailUser(){
+        addressRepository.updateDetailUser("1666","1256");
     }
 }
