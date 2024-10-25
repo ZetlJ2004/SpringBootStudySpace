cab4ee7c2f508e2b350d020f8a13ba0049d0caf8
ZetlJ2004 <1374691379@qq.com>
2024-10-25 18:50:23 +0800

    exp4-02

---
+++ springmvc-examples/src/main/java/org/example/springmvcexamples/controller/UserController.java
@@ -0,0 +1,2 @@
+package org.example.springmvcexamples.controller;public class UserController {
+}
---
+++ springmvc-examples/src/main/java/org/example/springmvcexamples/interceptor/AdminInterceptor.java
@@ -0,0 +1,2 @@
+package org.example.springmvcexamples.interceptor;public class AdminInterceptor {
+}
